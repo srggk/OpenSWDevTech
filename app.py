fe9d7674f4f45869b9c9efeb4a0f3d7608384273
Sergey K <106663310+srggk@users.noreply.github.com>
2023-10-11 19:39:22 +0500

    add pagination

--- app.py
+++ app.py
@@ -3,14 +3,37 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def pokemons():
-    response = requests.get('https://pokeapi.co/api/v2/pokemon/?limit=1')
+limit = 12
+
+
+def get_poke(offset=0):
+    url = f"https://pokeapi.co/api/v2/pokemon/?offset={offset}&limit={limit}"
+    response = requests.get(url)
+
     if response.status_code == 200:
-        pokemons_count = response.json()['count']
-        all_pokemons_data = requests.get(f'https://pokeapi.co/api/v2/pokemon/?limit={pokemons_count}').json()['results']
-        pokemons_name = [poke['name'] for poke in all_pokemons_data]
-    return render_template('index.html', pokemons=pokemons_name)
+        data = response.json()  
+        poke_list = data.get("results", [])
+        poke_count = data['count']
+        return poke_list, int(poke_count / limit + (1 if poke_count % limit > 0 else 0))
+    else:
+        print(response.status_code)
+        return [], 0
+
+
+@app.route('/')
+def poke():
+    page = 1
+    if request.args.get('page'):
+        if request.args.get('page').isdigit():
+            page = int(request.args.get('page'))
+
+    offset = (page - 1) * limit 
+    poke_list, page_count = get_poke(offset)
+
+    return render_template('index.html', 
+                           pokemons=poke_list, 
+                           current=page, 
+                           end=page_count)
 
 
 @app.route('/search', methods=['POST'])
