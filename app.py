db1c974d9ba112ad36010f9b61dbe6a2e9c147c6
Sergey K <106663310+srggk@users.noreply.github.com>
2023-10-12 12:35:41 +0500

    update poke card & add more info about poke and poke page & fix search

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-limit = 12
+limit = 10
 
 
 def get_poke(offset=0, limit=limit):
@@ -18,6 +18,37 @@ def get_poke(offset=0, limit=limit):
     else:
         print(response.status_code)
         return [], 0
+    
+
+def get_poke_info(poke_name):
+    if poke_name:
+        url = f"https://pokeapi.co/api/v2/pokemon/{poke_name.strip()}/"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            stats = data['stats']
+            hp = next((x for x in stats if x['stat']['name'] == 'hp'), None)
+            attack = next((x for x in stats if x['stat']['name'] == 'attack'), None)
+            defense = next((x for x in stats if x['stat']['name'] == 'defense'), None)
+            special_attack = next((x for x in stats if x['stat']['name'] == 'special-attack'), None)
+            special_defense = next((x for x in stats if x['stat']['name'] == 'special-defense'), None)
+            speed = next((x for x in stats if x['stat']['name'] == 'speed'), None)
+
+            return {
+                'name': data['name'],
+                'image': data['sprites']['front_default'],
+                'hp': hp['base_stat'] if hp else 0,
+                'attack': attack['base_stat'] if attack else 0,
+                'defense': defense['base_stat'] if defense else 0,
+                'special_attack': special_attack['base_stat'] if special_attack else 0,
+                'special_defense': special_defense['base_stat'] if special_defense else 0,
+                'speed': speed['base_stat'] if speed else 0,
+            }
+    else:
+        print('404 poke info')
+        return {}
 
 
 @app.route('/')
@@ -38,11 +69,29 @@ def poke():
         page_count = int(len(poke_list) / limit + (1 if len(poke_list) % limit > 0 else 0))
         poke_list = poke_list[offset : offset + limit]
 
+    poke_list = [get_poke_info(pkm['name']) for pkm in poke_list]
+
     return render_template('index.html', 
                            pokemons=poke_list, 
                            search_string=search_string,
                            current=page, 
                            end=page_count)
+
+
+@app.route("/poke/<string:poke_name>")
+def poke_page(poke_name):
+    poke = get_poke_info(poke_name)
+    return render_template('info.html', poke=poke)
+
+
+@app.route('/battle', methods=['GET', 'POST'])
+def battle():
+    if request.method == 'POST':
+        select_poke = request.form['select_poke']
+        return render_template('battle.html',
+                               select_poke=select_poke)
+    
+    return redirect(url_for('poke'))
 
 
 if __name__ == '__main__':
